cd2f0f3a884ae4d84e174e45d84e18a81d19b31c
Nate Woolls <nwoolls@gmail.com>
2013-06-29 15:07:52 -0400

    Refactoring the device list parser

--- MultiMiner.Xgminer/DeviceParser.cs
+++ MultiMiner.Xgminer/DeviceParser.cs
@@ -9,12 +9,57 @@ namespace MultiMiner.Xgminer
 {
     public static class DeviceParser
     {
-        public static void ParseOutputForDevices(List<string> output, List<Device> result)
+        public static void ParseOutputForDevices(List<string> output, List<Device> devices)
         {
-            ParseOutputForDevicePlatforms(output, result);
+            ParseOutputForGpuDevices(output, devices);
+            ParseOutputForUsbDevices(output, devices);
         }
 
-        private static void ParseOutputForDevicePlatforms(List<string> output, List<Device> result)
+        private static void ParseOutputForUsbDevices(List<string> output, List<Device> devices)
+        {
+            bool inUsbList = false;
+
+            string currentUsbManufacturer = string.Empty;
+            string currentUsbProduct = string.Empty;
+
+            foreach (string line in output)
+            {
+                if (Regex.Match(line, DevicePatterns.UsbDevicesFooter).Success)
+                {
+                    inUsbList = false;
+                }
+
+                if (inUsbList)
+                {
+                    Match match = Regex.Match(line, DevicePatterns.UsbManufacturer);
+                    if (match.Success)
+                        currentUsbManufacturer = match.Groups[1].Value.TrimEnd();
+
+                    match = Regex.Match(line, DevicePatterns.UsbProduct);
+                    if (match.Success)
+                    {
+                        currentUsbProduct = match.Groups[1].Value.TrimEnd();
+
+                        Device device = new Device();
+                        device.Platform = string.Empty;
+                        device.Vendor = currentUsbManufacturer;
+                        device.Version = string.Empty;
+                        device.Name = currentUsbProduct;
+                        device.Description = string.Empty;
+                        device.Kind = DeviceKind.USB;
+
+                        devices.Add(device);
+                    }
+                }
+
+                if (Regex.Match(line, DevicePatterns.UsbDevicesHeader).Success)
+                {
+                    inUsbList = true;
+                }
+            }
+        }
+
+        private static void ParseOutputForGpuDevices(List<string> output, List<Device> devices)
         {
             bool inPlatform = false;
 
@@ -24,46 +69,36 @@ namespace MultiMiner.Xgminer
             string currentDeviceName = string.Empty;
             string currentDeviceDescription = string.Empty;
 
-            const string platformVendorPattern = @"\[.+ .+\] \w* Platform \d+ vendor: (.*)";
-            const string platformNamePattern = @"\[.+ .+\] \w* Platform \d+ name: (.*)";
-            const string platformVersionPattern = @"\[.+ .+\] \w* Platform \d+ version: (.*)";
-
-            const string platformDevicesHeaderPattern = @"\[.+ .+\] Platform \d+ devices: \d+";
-            const string platformDevicesFooterPattern = @"\[.+ .+\] \d+ \w+ devices max detected";
-
-            const string deviceNamePattern = @"\[.+ .+\] \t\d+\t(.*)";
-            const string deviceDescriptionPattern = @"\[.+ .+\] \w* \d+ (.*)";
-
             List<string> names = new List<string>();
             List<string> descriptions = new List<string>();
 
             foreach (string line in output)
             {
-                Match match = Regex.Match(line, platformVendorPattern);
+                Match match = Regex.Match(line, DevicePatterns.PlatformVendor);
                 if (match.Success)
                     currentPlatformVendor = match.Groups[1].Value.TrimEnd();
 
-                match = Regex.Match(line, platformNamePattern);
+                match = Regex.Match(line, DevicePatterns.PlatformName);
                 if (match.Success)
                     currentPlatformName = match.Groups[1].Value.TrimEnd();
 
-                match = Regex.Match(line, platformVersionPattern);
+                match = Regex.Match(line, DevicePatterns.PlatformVersion);
                 if (match.Success)
                     currentPlatformVersion = match.Groups[1].Value.TrimEnd();
 
-                if (Regex.Match(line, platformDevicesFooterPattern).Success)
+                if (Regex.Match(line, DevicePatterns.PlatformDevicesFooter).Success)
                 {
                     for (int i = 0; i < names.Count; i++)
                     {
                         Device device = new Device();
-                        device.Platform.Name = currentPlatformName;
-                        device.Platform.Vendor = currentPlatformVendor;
-                        device.Platform.Version = currentPlatformVersion;
+                        device.Platform = currentPlatformName;
+                        device.Vendor = currentPlatformVendor;
+                        device.Version = currentPlatformVersion;
                         device.Name = names[i];
                         device.Description = descriptions[i];
                         device.Kind = DeviceKind.GPU;
 
-                        result.Add(device);
+                        devices.Add(device);
                     }
 
                     inPlatform = false;
@@ -73,7 +108,7 @@ namespace MultiMiner.Xgminer
 
                 if (inPlatform)
                 {
-                    match = Regex.Match(line, deviceNamePattern);
+                    match = Regex.Match(line, DevicePatterns.DeviceName);
                     if (match.Success)
                     {
                         currentDeviceName = match.Groups[1].Value.TrimEnd();
@@ -81,7 +116,7 @@ namespace MultiMiner.Xgminer
                     }
 
 
-                    match = Regex.Match(line, deviceDescriptionPattern);
+                    match = Regex.Match(line, DevicePatterns.DeviceDescription);
                     if (match.Success)
                     {
                         currentDeviceDescription = match.Groups[1].Value.TrimEnd();
@@ -90,14 +125,12 @@ namespace MultiMiner.Xgminer
 
                 }
 
-                if (Regex.Match(line, platformDevicesHeaderPattern).Success)
+                if (Regex.Match(line, DevicePatterns.PlatformDevicesHeader).Success)
                 {
                     inPlatform = true;
                     names.Clear();
                     descriptions.Clear();
                 }
-
-                // do something with line
             }
         }
     }
---
+++ MultiMiner.Xgminer/DevicePatterns.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiMiner.Xgminer
+{
+    public static class DevicePatterns
+    {
+        public const string PlatformVendor = @"\[.+ .+\] \w* Platform \d+ vendor: (.*)";
+        public const string PlatformName = @"\[.+ .+\] \w* Platform \d+ name: (.*)";
+        public const string PlatformVersion = @"\[.+ .+\] \w* Platform \d+ version: (.*)";
+        public const string PlatformDevicesHeader = @"\[.+ .+\] Platform \d+ devices: \d+";
+        public const string PlatformDevicesFooter = @"\[.+ .+\] \d+ \w+ devices max detected";
+        public const string DeviceName = @"\[.+ .+\] \t\d+\t(.*)";
+        public const string DeviceDescription = @"\[.+ .+\] \w* \d+ (.*)";
+        public const string UsbDevicesHeader = @"\[.+ .+\] USB all: found .*";
+        public const string UsbDevicesFooter = @"\[.+ .+\] \d+ known USB devices";
+        public const string UsbManufacturer = @" Manufacturer: '(.*)'";
+        public const string UsbProduct = @" Product: '(.*)'";
+    }
+}
