b64576764253bebde931e27b12498360ac643020
Nate Woolls <nwoolls@gmail.com>
2013-06-30 16:54:38 -0400

    Introuced a new assembly for reading Coinchoose info

---
+++ MultiMiner.Coinchoose.Api/ApiContext.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiMiner.Coinchoose.Api
+{
+    public static class ApiContext
+    {
+        public static JArray GetCoinInformation()
+        {
+            WebClient client = new WebClient();
+            string s = client.DownloadString("http://www.coinchoose.com/api.php");
+            JArray jsonArray = JArray.Parse(s);
+            return jsonArray;
+        }
+    }
+}
