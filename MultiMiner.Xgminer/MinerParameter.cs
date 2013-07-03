049761650f106a931160fb6e26654861f07c6e00
Nate Woolls <nwoolls@gmail.com>
2013-07-02 22:17:49 -0400

    Removed unused constants

--- MultiMiner.Xgminer/MinerParameter.cs
+++ MultiMiner.Xgminer/MinerParameter.cs
@@ -2,9 +2,6 @@
 {
     public static class MinerParameter
     {
-        public const string DisableGpu = "--disable-gpu";
-        public const string DisableUsb = "--usb :0";
-        public const string Benchmark = "--benchmark";
         public const string ApiListen = "--api-listen";
         public const string EnumerateDevices = "--ndevs";
     }
