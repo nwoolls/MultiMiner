d4b7b40c82f9715c74c4ea5c38b60bf7e774cfd3
Nate Woolls <nwoolls@gmail.com>
2013-07-03 12:49:10 -0400

    Return a strongly typed list from GetCoinInformation() rather than a JSON array

--- MultiMiner.Coinchoose.Api/ApiContext.cs
+++ MultiMiner.Coinchoose.Api/ApiContext.cs
@@ -1,16 +1,30 @@
 ﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace MultiMiner.Coinchoose.Api
 {
     public static class ApiContext
     {
-        public static JArray GetCoinInformation()
+        public static List<CoinInformation> GetCoinInformation()
         {
             WebClient client = new WebClient();
             string jsonString = client.DownloadString("http://www.coinchoose.com/api.php");
             JArray jsonArray = JArray.Parse(jsonString);
-            return jsonArray;
+
+            List<CoinInformation> result = new List<CoinInformation>();
+
+            foreach (JToken jToken in jsonArray)
+            {
+                CoinInformation coinInformation = new CoinInformation();
+
+                coinInformation.PopulateFromJson(jToken);
+
+                result.Add(coinInformation);
+            }
+
+            return result;
         }
     }
 }
---
+++ MultiMiner.Coinchoose.Api/CoinInformation.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiMiner.Coinchoose.Api
+{
+    public class CoinInformation
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string Algorithm { get; set; }
+        public int CurrentBlocks { get; set; }
+        public double Difficulty { get; set; }
+        public double Reward { get; set; }
+        public double MinimumBlockTime { get; set; }
+        public Int64 NetworkHashRate { get; set; }
+        public double Price { get; set; }
+        public string Exchange { get; set; }
+        public double Profitability { get; set; }
+        public double AdjustedProfitability { get; set; }
+        public double AverageProfitability { get; set; }
+        public double AverageHashRate { get; set; }
+
+        public void PopulateFromJson(JToken jToken)
+        {
+            Symbol = jToken.Value<string>("symbol");
+            Name = jToken.Value<string>("name");
+            Algorithm = jToken.Value<string>("algo");
+            CurrentBlocks = jToken.Value<int>("currentBlocks");
+            Difficulty = jToken.Value<double>("difficulty");
+            Reward = jToken.Value<double>("reward");
+            MinimumBlockTime = jToken.Value<double>("minBlockTime");
+            NetworkHashRate = jToken.Value<Int64>("networkhashrate");
+            Price = jToken.Value<double>("price");
+            Exchange = jToken.Value<string>("exchange");
+            Profitability = jToken.Value<double>("ratio");
+            AdjustedProfitability = jToken.Value<double>("adjustedratio");
+            AverageProfitability = jToken.Value<double>("avgProfit");
+            AverageHashRate = jToken.Value<double>("avgHash");
+        }
+    }
+}
