0d66d32ee2a8323799ef733c2008940754aa3e54
Nate Woolls <nwoolls@gmail.com>
2013-07-05 12:49:39 -0400

    Retain the API context so it can be used to ID processes later

--- MultiMiner.Engine/MinerProcess.cs
+++ MultiMiner.Engine/MinerProcess.cs
@@ -11,23 +11,30 @@ namespace MultiMiner.Engine
         public int ApiPort { get; set; }
         public MinerConfiguration MinerConfiguration { get; set; }
 
+        private ApiContext apiContext;
         public ApiContext ApiContext 
         { 
             get 
             {
-                ApiContext result = null;
-                try
-                {
-                    result = new ApiContext(ApiPort);
-                }
-                catch (SocketException ex)
-                {
-                    //won't be able to connect for the first 5s or so
-                    result = null;
-                }
-                return result;
+                if (apiContext == null)
+                    apiContext = GetApiContext();
+                return apiContext;
             } 
         }
 
+        private ApiContext GetApiContext()
+        {
+            ApiContext result = null;
+            try
+            {
+                result = new ApiContext(ApiPort);
+            }
+            catch (SocketException ex)
+            {
+                //won't be able to connect for the first 5s or so
+                result = null;
+            }
+            return result;
+        }
     }
 }
