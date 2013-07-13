bfe29b2d182e1d545ecdea58d65acdcbf70c3e94
Nate Woolls <nwoolls@gmail.com>
2013-07-13 02:35:57 -0400

    Adjusting dialog size

--- MultiMiner.Win/ProgressForm.Designer.cs
+++ MultiMiner.Win/ProgressForm.Designer.cs
@@ -66,7 +66,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(308, 72);
+            this.ClientSize = new System.Drawing.Size(308, 80);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
@@ -78,7 +78,6 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Progress";
-            
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
