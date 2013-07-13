4cf25b135e31afcc6a5e0a62d06e188e4afb34c7
Nate Woolls <nwoolls@gmail.com>
2013-07-13 02:35:41 -0400

    Added static method InstallMiner() - downloads & installs latest cgminer

--- MultiMiner.Xgminer/Miner.cs
+++ MultiMiner.Xgminer/Miner.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace MultiMiner.Xgminer
@@ -149,6 +152,63 @@ namespace MultiMiner.Xgminer
                 Thread.Sleep(timeout);
                 retries++;
             }
+        }
+
+        private const string cgminerDomain = "ck.kolivas.org";
+
+        public static void InstallMiner(MinerBackend minerBackend, string destinationFolder)
+        {
+            if (minerBackend == MinerBackend.Bfgminer)
+                throw new NotImplementedException();
+
+            string minerUrl = GetLatestCgminerDownloadUrl();
+            if (!String.IsNullOrEmpty(minerUrl))
+            {
+                string minerDownloadFile = Path.Combine(Environment.GetEnvironmentVariable("TEMP"), "miner.zip");
+                File.Delete(minerDownloadFile);
+                
+                new WebClient().DownloadFile(new Uri(minerUrl), minerDownloadFile);
+                try
+                {
+                    UnzipFileToFolder(minerDownloadFile, destinationFolder, false, true);
+                }
+                finally
+                {
+                    File.Delete(minerDownloadFile);
+                }
+            }
+        }
+
+        private static string GetLatestCgminerDownloadUrl()
+        {
+            string availableDownloadsHtml = new WebClient().DownloadString("http://" + cgminerDomain + "/apps/cgminer/");
+            const string pattern = @".*<a href=""(cgminer-.+?-windows.zip)";
+            Match match = Regex.Match(availableDownloadsHtml, pattern);
+            if (match.Success)
+            {
+                string minerFileName = match.Groups[1].Value;
+                string minerUrl = String.Format("http://ck.kolivas.org/apps/cgminer/{0}", minerFileName);
+                return minerUrl;
+            }
+
+            return "";
+        }
+
+        private static void UnzipFileToFolder(string zipFilePath, string destionationFolder, bool showProgress, bool yesToAll)
+        {
+            Shell32.ShellClass shellClass = new Shell32.ShellClass();
+            Shell32.Folder sourceFolder = shellClass.NameSpace(zipFilePath);
+            Shell32.Folder destinationFolder = shellClass.NameSpace(destionationFolder);
+            Shell32.FolderItems sourceFolderItems = sourceFolder.Items();
+            Shell32.FolderItem rootItem = sourceFolderItems.Item(0);
+
+            int options = 0;
+            if (!showProgress)
+                options += 4;
+            if (yesToAll)
+                options += 16;
+
+            destinationFolder.CopyHere(((Shell32.Folder)rootItem.GetFolder).Items(), options);
         }
     }
 }
