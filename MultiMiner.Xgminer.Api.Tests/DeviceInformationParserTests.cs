2b56465b9c2e09d9a7dc303870319d89b227072e
Nate Woolls <nwoolls@gmail.com>
2013-07-14 15:59:48 -0400

    Unit test added for DeviceInformationParser

---
+++ MultiMiner.Xgminer.Api.Tests/DeviceInformationParserTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using MultiMiner.Xgminer.Api.Parsers;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MultiMiner.Xgminer.Api.Tests
+{
+    [TestClass]
+    public class DeviceInformationParserTests
+    {
+        [TestMethod]
+        public void ParseText_RussianCulture_Succeeds()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+            const string apiText = @"STATUS=S,When=1373818935,Code=9,Msg=2 GPU(s) - 0 ASC(s) - 0 PGA(s) - ,Description=cgminer 3.3.1|GPU=0,Enabled=Y,Status=Alive,Temperature=66.00,Fan Speed=3257,Fan Percent=70,GPU Clock=1065,Memory Clock=1650,GPU Voltage=1.090,GPU Activity=99,Powertune=0,MHS av=0.64,MHS 1s=0.58,Accepted=24,Rejected=0,Hardware Errors=0,Utility=145.71,Intensity=20,Last Share Pool=0,Last Share Time=1373818934,Total MH=6.2915,Diff1 Work=85,Difficulty Accepted=90.00000000,Difficulty Rejected=0.00000000,Last Share Difficulty=2.00000000,Last Valid Work=1373818934|GPU=1,Enabled=Y,Status=Alive,Temperature=75.00,Fan Speed=3242,Fan Percent=70,GPU Clock=1065,Memory Clock=1650,GPU Voltage=1.090,GPU Activity=99,Powertune=0,MHS av=0.64,MHS 1s=0.58,Accepted=30,Rejected=0,Hardware Errors=0,Utility=182.14,Intensity=20,Last Share Pool=0,Last Share Time=1373818934,Total MH=6.2915,Diff1 Work=95,Difficulty Accepted=102.00000000,Difficulty Rejected=0.00000000,Last Share Difficulty=2.00000000,Last Valid Work=1373818934|";
+            List<DeviceInformation> deviceInformation = new List<DeviceInformation>();
+            DeviceInformationParser.ParseTextForDeviceInformation(apiText, deviceInformation);
+        }
+    }
+}
---
+++ MultiMiner.Xgminer.Api.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MultiMiner.Xgminer.Api.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MultiMiner.Xgminer.Api.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a2357f9-44fc-4cc5-9c73-8e72d984f075")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
