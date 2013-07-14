ebee059f71901ae99bc760f5d981c6353a9573f0
Nate Woolls <nwoolls@gmail.com>
2013-07-14 15:59:14 -0400

    Fixes for device parsing - handle keyValuePairs.Count == 0 and "MHS 1s"

--- MultiMiner.Xgminer.Api/Parsers/DeviceInformationParser.cs
+++ MultiMiner.Xgminer.Api/Parsers/DeviceInformationParser.cs
@@ -23,43 +23,52 @@ namespace MultiMiner.Xgminer.Api.Parsers
                   .Select(value => value.Split('='))
                   .ToDictionary(pair => pair[0], pair => pair[1]);
 
-                DeviceInformation newDevice = new DeviceInformation();
-
-                newDevice.Kind = keyValuePairs.ElementAt(0).Key;
-                newDevice.Index = int.Parse(keyValuePairs[newDevice.Kind]);
-                newDevice.Enabled = keyValuePairs["Enabled"].Equals("Y");
-                newDevice.Status = keyValuePairs["Status"];
-
-                //the RPC API returns numbers formatted en-US, e.g. 1,000.00
-                //specify CultureInfo.InvariantCulture for parsing or unhandled exceptions will
-                //occur on other locales
-                //can test for this with:
-                //Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
-                //Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
-
-                if (newDevice.Kind.Equals("GPU"))
+                //seen Count == 0 with user API logs
+                if (keyValuePairs.Count > 0)
                 {
-                    if (keyValuePairs.ContainsKey("Temperature")) //check required for bfgminer
-                        newDevice.Temperature = double.Parse(keyValuePairs["Temperature"], CultureInfo.InvariantCulture);
-                    newDevice.FanSpeed = int.Parse(keyValuePairs["Fan Speed"], CultureInfo.InvariantCulture);
-                    newDevice.FanPercent = int.Parse(keyValuePairs["Fan Percent"], CultureInfo.InvariantCulture);
-                    newDevice.GpuClock = int.Parse(keyValuePairs["GPU Clock"], CultureInfo.InvariantCulture);
-                    newDevice.MemoryClock = int.Parse(keyValuePairs["Memory Clock"], CultureInfo.InvariantCulture);
-                    newDevice.GpuVoltage = double.Parse(keyValuePairs["GPU Voltage"], CultureInfo.InvariantCulture);
-                    newDevice.GpuActivity = int.Parse(keyValuePairs["GPU Activity"], CultureInfo.InvariantCulture);
-                    newDevice.PowerTune = int.Parse(keyValuePairs["Powertune"], CultureInfo.InvariantCulture);
-                    newDevice.Intensity = keyValuePairs["Intensity"];
+                    DeviceInformation newDevice = new DeviceInformation();
+
+                    newDevice.Kind = keyValuePairs.ElementAt(0).Key;
+                    newDevice.Index = int.Parse(keyValuePairs[newDevice.Kind]);
+                    newDevice.Enabled = keyValuePairs["Enabled"].Equals("Y");
+                    newDevice.Status = keyValuePairs["Status"];
+
+                    //the RPC API returns numbers formatted en-US, e.g. 1,000.00
+                    //specify CultureInfo.InvariantCulture for parsing or unhandled exceptions will
+                    //occur on other locales
+                    //can test for this with:
+                    //Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+                    //Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+
+                    if (newDevice.Kind.Equals("GPU"))
+                    {
+                        if (keyValuePairs.ContainsKey("Temperature")) //check required for bfgminer
+                            newDevice.Temperature = double.Parse(keyValuePairs["Temperature"], CultureInfo.InvariantCulture);
+                        newDevice.FanSpeed = int.Parse(keyValuePairs["Fan Speed"], CultureInfo.InvariantCulture);
+                        newDevice.FanPercent = int.Parse(keyValuePairs["Fan Percent"], CultureInfo.InvariantCulture);
+                        newDevice.GpuClock = int.Parse(keyValuePairs["GPU Clock"], CultureInfo.InvariantCulture);
+                        newDevice.MemoryClock = int.Parse(keyValuePairs["Memory Clock"], CultureInfo.InvariantCulture);
+                        newDevice.GpuVoltage = double.Parse(keyValuePairs["GPU Voltage"], CultureInfo.InvariantCulture);
+                        newDevice.GpuActivity = int.Parse(keyValuePairs["GPU Activity"], CultureInfo.InvariantCulture);
+                        newDevice.PowerTune = int.Parse(keyValuePairs["Powertune"], CultureInfo.InvariantCulture);
+                        newDevice.Intensity = keyValuePairs["Intensity"];
+                    }
+
+                    newDevice.AverageHashrate = double.Parse(keyValuePairs["MHS av"], CultureInfo.InvariantCulture) * 1000;
+                    
+                    //seen both MHS 5s and MHS 1s
+                    if (keyValuePairs.ContainsKey("MHS 5s"))
+                        newDevice.CurrentHashrate = double.Parse(keyValuePairs["MHS 5s"], CultureInfo.InvariantCulture) * 1000;
+                    else if (keyValuePairs.ContainsKey("MHS 1s"))
+                        newDevice.CurrentHashrate = double.Parse(keyValuePairs["MHS 1s"], CultureInfo.InvariantCulture) * 1000;
+
+                    newDevice.AcceptedShares = int.Parse(keyValuePairs["Accepted"], CultureInfo.InvariantCulture);
+                    newDevice.RejectedShares = int.Parse(keyValuePairs["Rejected"], CultureInfo.InvariantCulture);
+                    newDevice.HardwareErrors = int.Parse(keyValuePairs["Hardware Errors"], CultureInfo.InvariantCulture);
+                    newDevice.Utility = double.Parse(keyValuePairs["Utility"], CultureInfo.InvariantCulture);
+
+                    deviceInformation.Add(newDevice);
                 }
-
-                newDevice.AverageHashrate = double.Parse(keyValuePairs["MHS av"], CultureInfo.InvariantCulture) * 1000;
-                newDevice.CurrentHashrate = double.Parse(keyValuePairs["MHS 5s"], CultureInfo.InvariantCulture) * 1000;
-
-                newDevice.AcceptedShares = int.Parse(keyValuePairs["Accepted"], CultureInfo.InvariantCulture);
-                newDevice.RejectedShares = int.Parse(keyValuePairs["Rejected"], CultureInfo.InvariantCulture);
-                newDevice.HardwareErrors = int.Parse(keyValuePairs["Hardware Errors"], CultureInfo.InvariantCulture);
-                newDevice.Utility = double.Parse(keyValuePairs["Utility"], CultureInfo.InvariantCulture);
-
-                deviceInformation.Add(newDevice);
             }
         }
     }
