ae25db968778996e5971a0bdc447132fcb0a6218
Nate Woolls <nwoolls@gmail.com>
2013-07-21 00:53:16 -0400

    Added a new library for interacting with the MobileMiner API

---
+++ MultiMiner.MobileMiner.Api/ApiContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Xml.Serialization;
+
+namespace MultiMiner.MobileMiner.Api
+{
+    public class ApiContext
+    {
+        ////User
+        //public virtual string EmailAddress { get; set; }
+        //public virtual string ApplicationKey { get; set; }
+
+        static public void SubmitMiningStatistics(string url, MiningStatistics miningStatistics)
+        {
+            // http://local_ip/api/accounts
+
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/xml";
+            request.ContentLength = 800;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(MiningStatistics));
+            xmlSerializer.Serialize(request.GetRequestStream(), miningStatistics);
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(MiningStatistics));
+
+            var newRecord = (MiningStatistics)serializer.Deserialize(response.GetResponseStream());
+
+        }
+    }
+}
---
+++ MultiMiner.MobileMiner.Api/MiningStatistics.cs
@@ -0,0 +1,37 @@
+namespace MultiMiner.MobileMiner.Api
+{
+    public class MiningStatistics
+    {
+        //User
+        public virtual string EmailAddress { get; set; }
+        public virtual string ApplicationKey { get; set; }
+
+        //Machine
+        public virtual string MachineName { get; set; }
+
+        //MiningStatistics
+        public string CoinSymbol { get; set; }
+        public string CoinName { get; set; }
+
+        //rest is taken from MultiMiner.Xgminer.Api.DeviceInformation
+        public string Kind { get; set; }
+        public int Index { get; set; }
+        public bool Enabled { get; set; }
+        public string Status { get; set; }
+        public double Temperature { get; set; }
+        public int FanSpeed { get; set; }
+        public int FanPercent { get; set; }
+        public int GpuClock { get; set; }
+        public int MemoryClock { get; set; }
+        public double GpuVoltage { get; set; }
+        public int GpuActivity { get; set; }
+        public int PowerTune { get; set; }
+        public double AverageHashrate { get; set; }
+        public double CurrentHashrate { get; set; }
+        public int AcceptedShares { get; set; }
+        public int RejectedShares { get; set; }
+        public int HardwareErrors { get; set; }
+        public double Utility { get; set; }
+        public string Intensity { get; set; } //string, might be D
+    }
+}
