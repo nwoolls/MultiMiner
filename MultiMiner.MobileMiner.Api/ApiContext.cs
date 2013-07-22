b72abcc06abeace43a227b217b1b7cd17e5bdbb2
Nate Woolls <nwoolls@gmail.com>
2013-07-21 20:12:41 -0400

    Change POST code from XML to JSON, fixed up to work properly with WebAPI

--- MultiMiner.MobileMiner.Api/ApiContext.cs
+++ MultiMiner.MobileMiner.Api/ApiContext.cs
@@ -1,33 +1,20 @@
 ﻿using System.Net;
-using System.Xml.Serialization;
+using System.Web.Script.Serialization;
 
 namespace MultiMiner.MobileMiner.Api
 {
     public class ApiContext
     {
-        ////User
-        //public virtual string EmailAddress { get; set; }
-        //public virtual string ApplicationKey { get; set; }
-
         static public void SubmitMiningStatistics(string url, MiningStatistics miningStatistics)
         {
-            // http://local_ip/api/accounts
-
-
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "POST";
-            request.ContentType = "application/xml";
-            request.ContentLength = 800;
-
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(MiningStatistics));
-            xmlSerializer.Serialize(request.GetRequestStream(), miningStatistics);
-
-            var response = (HttpWebResponse)request.GetResponse();
-
-            XmlSerializer serializer = new XmlSerializer(typeof(MiningStatistics));
-
-            var newRecord = (MiningStatistics)serializer.Deserialize(response.GetResponseStream());
-
+            string fullUrl = url + "/api/MiningStatisticsInput";
+            using (WebClient client = new WebClient())
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                string jsonData = serializer.Serialize(miningStatistics);
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                client.UploadString(fullUrl, jsonData);
+            }
         }
     }
 }
