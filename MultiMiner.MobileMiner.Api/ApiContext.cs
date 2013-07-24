0845dadd6b5bb415255c6ad7a91d1ecc6d1d9c03
Nate Woolls <nwoolls@gmail.com>
2013-07-24 18:27:35 -0400

    Added support for getting and deleting remote commands

--- MultiMiner.MobileMiner.Api/ApiContext.cs
+++ MultiMiner.MobileMiner.Api/ApiContext.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Net;
 using System.Web.Script.Serialization;
+using System;
 
 namespace MultiMiner.MobileMiner.Api
 {
     public class ApiContext
     {
-        static public void SubmitMiningStatistics(string url, List<MiningStatistics> miningStatistics)
+        public static void SubmitMiningStatistics(string url, List<MiningStatistics> miningStatistics)
         {
             if (!url.EndsWith("/"))
                 url = url + "/";
@@ -16,7 +17,35 @@ namespace MultiMiner.MobileMiner.Api
                 JavaScriptSerializer serializer = new JavaScriptSerializer();
                 string jsonData = serializer.Serialize(miningStatistics);
                 client.Headers[HttpRequestHeader.ContentType] = "application/json";
-                client.UploadString(fullUrl, jsonData);
+                string response = client.UploadString(fullUrl, jsonData);
+            }
+        }
+
+        public static List<RemoteCommand> GetCommands(string url, string emailAddress, string applicationKey, string machineName)
+        {
+            if (!url.EndsWith("/"))
+                url = url + "/";
+            string fullUrl = String.Format("{0}api/RemoteCommands?emailAddress={1}&applicationKey={2}&machineName={3}", 
+                url, emailAddress, applicationKey, machineName);
+            using (WebClient client = new WebClient())
+            {
+                string response = client.DownloadString(fullUrl);
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                return serializer.Deserialize<List<RemoteCommand>>(response);
+            }
+        }
+
+        public static RemoteCommand DeleteCommand(string url, string emailAddress, string applicationKey, string machineName, long commandId)
+        {
+            if (!url.EndsWith("/"))
+                url = url + "/";
+            string fullUrl = String.Format("{0}api/RemoteCommands?emailAddress={1}&applicationKey={2}&machineName={3}&commandId={4}",
+                url, emailAddress, applicationKey, machineName, commandId);
+            using (WebClient client = new WebClient())
+            {
+                string response = client.UploadString(fullUrl, "DELETE", "");
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                return serializer.Deserialize<RemoteCommand>(response);
             }
         }
     }
---
+++ MultiMiner.MobileMiner.Api/RemoteCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiMiner.MobileMiner.Api
+{
+    public class RemoteCommand
+    {
+        public int Id { get; set; }
+        public string CommandText { get; set; }
+    }
+}
