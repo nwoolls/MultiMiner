0325b8eb0f9b095dc330dc5b2f3d4f6dd747fcec
Nate Woolls <nwoolls@gmail.com>
2013-08-26 13:45:53 -0400

    Add controls for adjusting the new miner backend API settings

--- MultiMiner.Win/AdvancedSettingsForm.Designer.cs
+++ MultiMiner.Win/AdvancedSettingsForm.Designer.cs
@@ -28,13 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdvancedSettingsForm));
             this.panel1 = new System.Windows.Forms.Panel();
             this.saveButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.erupterCheckBox = new System.Windows.Forms.CheckBox();
+            this.xgminerConfigurationBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.disableGpuCheckbox = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.xgminerConfigurationBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -42,15 +52,15 @@
             this.panel1.Controls.Add(this.saveButton);
             this.panel1.Controls.Add(this.cancelButton);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 197);
+            this.panel1.Location = new System.Drawing.Point(0, 173);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(403, 47);
+            this.panel1.Size = new System.Drawing.Size(392, 47);
             this.panel1.TabIndex = 4;
             // 
             // saveButton
             // 
             this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.saveButton.Location = new System.Drawing.Point(235, 12);
+            this.saveButton.Location = new System.Drawing.Point(224, 12);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(75, 23);
             this.saveButton.TabIndex = 0;
@@ -62,18 +72,20 @@
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(316, 12);
+            this.cancelButton.Location = new System.Drawing.Point(305, 12);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 1;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
+            
             // 
             // erupterCheckBox
             // 
             this.erupterCheckBox.AutoSize = true;
             this.erupterCheckBox.Checked = true;
             this.erupterCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.erupterCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this.xgminerConfigurationBindingSource, "ErupterDriver", true));
             this.erupterCheckBox.Location = new System.Drawing.Point(12, 35);
             this.erupterCheckBox.Name = "erupterCheckBox";
             this.erupterCheckBox.Size = new System.Drawing.Size(199, 17);
@@ -81,9 +93,14 @@
             this.erupterCheckBox.Text = "Erupter-specific driver (bfgminer only)";
             this.erupterCheckBox.UseVisualStyleBackColor = true;
             // 
+            // xgminerConfigurationBindingSource
+            // 
+            this.xgminerConfigurationBindingSource.DataSource = typeof(MultiMiner.Engine.Configuration.XgminerConfiguration);
+            // 
             // disableGpuCheckbox
             // 
             this.disableGpuCheckbox.AutoSize = true;
+            this.disableGpuCheckbox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this.xgminerConfigurationBindingSource, "DisableGpu", true));
             this.disableGpuCheckbox.Location = new System.Drawing.Point(12, 12);
             this.disableGpuCheckbox.Name = "disableGpuCheckbox";
             this.disableGpuCheckbox.Size = new System.Drawing.Size(120, 17);
@@ -91,11 +108,70 @@
             this.disableGpuCheckbox.Text = "Disable GPU mining";
             this.disableGpuCheckbox.UseVisualStyleBackColor = true;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(368, 111);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "API Settings";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(102, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "127.0.0.1 is assumed/implied";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Permitted IPs:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.xgminerConfigurationBindingSource, "AllowedApiIps", true));
+            this.textBox2.Location = new System.Drawing.Point(105, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(245, 20);
+            this.textBox2.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Starting port:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.xgminerConfigurationBindingSource, "StartingApiPort", true));
+            this.textBox1.Location = new System.Drawing.Point(105, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 20);
+            this.textBox1.TabIndex = 24;
+            // 
             // AdvancedSettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(403, 244);
+            this.ClientSize = new System.Drawing.Size(392, 220);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.erupterCheckBox);
             this.Controls.Add(this.disableGpuCheckbox);
             this.Controls.Add(this.panel1);
@@ -107,6 +183,9 @@
             this.Text = "Advanced Miner Settings";
             this.Load += new System.EventHandler(this.AdvancedSettingsForm_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.xgminerConfigurationBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,5 +198,12 @@
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.CheckBox erupterCheckBox;
         private System.Windows.Forms.CheckBox disableGpuCheckbox;
+        private System.Windows.Forms.BindingSource xgminerConfigurationBindingSource;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- MultiMiner.Win/AdvancedSettingsForm.cs
+++ MultiMiner.Win/AdvancedSettingsForm.cs
@@ -1,47 +1,32 @@
 ﻿using MultiMiner.Engine.Configuration;
+using MultiMiner.Utility;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace MultiMiner.Win
 {
     public partial class AdvancedSettingsForm : Form
     {
-        XgminerConfiguration minerConfiguration;
+        readonly XgminerConfiguration minerConfiguration;
+        readonly XgminerConfiguration workingConfiguration;
 
         public AdvancedSettingsForm(XgminerConfiguration minerConfiguration)
         {
             InitializeComponent();
             this.minerConfiguration = minerConfiguration;
+            this.workingConfiguration = ObjectCopier.CloneObject<XgminerConfiguration, XgminerConfiguration>(minerConfiguration);
+        }
+
+        private void AdvancedSettingsForm_Load(object sender, EventArgs e)
+        {
+            xgminerConfigurationBindingSource.DataSource = workingConfiguration;
+            erupterCheckBox.Enabled = minerConfiguration.MinerBackend == Xgminer.MinerBackend.Bfgminer;
         }
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-            SaveSettings();
-
+            ObjectCopier.CopyObject(workingConfiguration, minerConfiguration);
             DialogResult = System.Windows.Forms.DialogResult.OK;
-        }
-        private void AdvancedSettingsForm_Load(object sender, EventArgs e)
-        {
-            LoadSettings();
-        }
-
-        private void LoadSettings()
-        {
-            disableGpuCheckbox.Checked = minerConfiguration.DisableGpu;
-            erupterCheckBox.Checked = minerConfiguration.ErupterDriver;
-            erupterCheckBox.Enabled = minerConfiguration.MinerBackend == Xgminer.MinerBackend.Bfgminer;
-        }
-
-        private void SaveSettings()
-        {
-            minerConfiguration.DisableGpu = disableGpuCheckbox.Checked;
-            minerConfiguration.ErupterDriver = erupterCheckBox.Checked;
         }
     }
 }
