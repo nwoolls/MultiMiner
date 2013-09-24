8e8927d7faa82bfbbd27e2b9025f34eae40c9299
Nate Woolls <nwoolls@gmail.com>
2013-09-23 20:02:48 -0400

    Set timeout for API calls to 10s (default was 100s)

--- MultiMiner.MobileMiner.Api/ApiContext.cs
+++ MultiMiner.MobileMiner.Api/ApiContext.cs
@@ -7,13 +7,26 @@ namespace MultiMiner.MobileMiner.Api
 {
     public class ApiContext
     {
+        private class ApiWebClient : WebClient
+        {
+            protected override WebRequest GetWebRequest(Uri uri)
+            {
+                WebRequest w = base.GetWebRequest(uri);
+                //default is 100s - far too long for our API calls
+                //if Azure is being flakey we don't want calls taking 100s to timeout
+                //lets go with 10s
+                w.Timeout = 10 * 1000;
+                return w;
+            }
+        }
+
         public static void SubmitMiningStatistics(string url, string apiKey, string emailAddress, string applicationKey, string machineName, List<MiningStatistics> miningStatistics)
         {
             if (!url.EndsWith("/"))
                 url = url + "/";
             string fullUrl = String.Format("{0}MiningStatisticsInput?emailAddress={1}&applicationKey={2}&machineName={3}&apiKey={4}", 
                 url, emailAddress, applicationKey, machineName, apiKey);
-            using (WebClient client = new WebClient())
+            using (WebClient client = new ApiWebClient())
             {
                 JavaScriptSerializer serializer = new JavaScriptSerializer();
                 string jsonData = serializer.Serialize(miningStatistics);
@@ -28,7 +41,7 @@ namespace MultiMiner.MobileMiner.Api
                 url = url + "/";
             string fullUrl = String.Format("{0}RemoteCommands?emailAddress={1}&applicationKey={2}&machineName={3}&apiKey={4}", 
                 url, emailAddress, applicationKey, machineName, apiKey);
-            using (WebClient client = new WebClient())
+            using (WebClient client = new ApiWebClient())
             {
                 string response = client.DownloadString(fullUrl);
                 JavaScriptSerializer serializer = new JavaScriptSerializer();
@@ -42,7 +55,7 @@ namespace MultiMiner.MobileMiner.Api
                 url = url + "/";
             string fullUrl = String.Format("{0}RemoteCommands?emailAddress={1}&applicationKey={2}&machineName={3}&commandId={4}&apiKey={5}",
                 url, emailAddress, applicationKey, machineName, commandId, apiKey);
-            using (WebClient client = new WebClient())
+            using (WebClient client = new ApiWebClient())
             {
                 string response = client.UploadString(fullUrl, "DELETE", "");
                 JavaScriptSerializer serializer = new JavaScriptSerializer();
