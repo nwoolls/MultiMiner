da0e581ca64e29a6327ec62c972c77088f67cf1d
Nate Woolls <nwoolls@gmail.com>
2013-09-24 15:25:42 -0400

    Recode reading device info for resiliance and reuse

--- MultiMiner.Xgminer.Api/Parsers/DeviceInformationParser.cs
+++ MultiMiner.Xgminer.Api/Parsers/DeviceInformationParser.cs
@@ -32,106 +32,84 @@ namespace MultiMiner.Xgminer.Api.Parsers
                     DeviceInformation newDevice = new DeviceInformation();
 
                     newDevice.Kind = keyValuePairs.ElementAt(0).Key;
-                                        
-                    //personally seen this need extra handling with a user
-                    string stringValue = keyValuePairs[newDevice.Kind];
-                    int intValue = 0;
-                    if (int.TryParse(stringValue, NumberStyles.Number,
-                             CultureInfo.InvariantCulture, out intValue))
-                        newDevice.Index = intValue;
-                    else
-                        continue; //no valid device index - no valid info
 
+                    newDevice.Index = TryToParseInt(keyValuePairs, newDevice.Kind, -1);
+                    if (newDevice.Index == -1)
+                        continue;
+                    
                     if (keyValuePairs.ContainsKey("Enabled")) //seen this needed with a user
                         newDevice.Enabled = keyValuePairs["Enabled"].Equals("Y");
 
                     if (keyValuePairs.ContainsKey("Status")) //check required for bfgminer
                         newDevice.Status = keyValuePairs["Status"];
 
-                    //the RPC API returns numbers formatted en-US, e.g. 1,000.00
-                    //specify CultureInfo.InvariantCulture for parsing or unhandled exceptions will
-                    //occur on other locales
-                    //can test for this with:
-                    //Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
-                    //Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
 
                     if (newDevice.Kind.Equals("GPU"))
                     {
-                        if (keyValuePairs.ContainsKey("Temperature")) //check required for bfgminer
-                            newDevice.Temperature = double.Parse(keyValuePairs["Temperature"], CultureInfo.InvariantCulture);
-                        newDevice.FanSpeed = int.Parse(keyValuePairs["Fan Speed"], CultureInfo.InvariantCulture);
-                        newDevice.FanPercent = int.Parse(keyValuePairs["Fan Percent"], CultureInfo.InvariantCulture);
-                        newDevice.GpuClock = int.Parse(keyValuePairs["GPU Clock"], CultureInfo.InvariantCulture);
-                        newDevice.MemoryClock = int.Parse(keyValuePairs["Memory Clock"], CultureInfo.InvariantCulture);
-                        newDevice.GpuVoltage = double.Parse(keyValuePairs["GPU Voltage"], CultureInfo.InvariantCulture);
-                        newDevice.GpuActivity = int.Parse(keyValuePairs["GPU Activity"], CultureInfo.InvariantCulture);
-                        newDevice.PowerTune = int.Parse(keyValuePairs["Powertune"], CultureInfo.InvariantCulture);
+                        newDevice.Temperature = TryToParseDouble(keyValuePairs, "Temperature", 0.00);
+                        newDevice.FanSpeed = TryToParseInt(keyValuePairs, "Fan Speed", 0);
+                        newDevice.FanPercent = TryToParseInt(keyValuePairs, "Fan Percent", 0);
+                        newDevice.GpuClock = TryToParseInt(keyValuePairs, "GPU Clock", 0);
+                        newDevice.MemoryClock = TryToParseInt(keyValuePairs, "Memory Clock", 0);
+                        newDevice.GpuVoltage = TryToParseDouble(keyValuePairs, "GPU Voltage", 0.00);
+                        newDevice.GpuActivity = TryToParseInt(keyValuePairs, "GPU Activity", 0);
+                        newDevice.PowerTune = TryToParseInt(keyValuePairs, "Powertune", 0);
                         newDevice.Intensity = keyValuePairs["Intensity"];
                     }
 
-                    if (keyValuePairs.ContainsKey("MHS av")) //check required for bfgminer
-                        newDevice.AverageHashrate = double.Parse(keyValuePairs["MHS av"], CultureInfo.InvariantCulture) * 1000;
+                    newDevice.AverageHashrate = TryToParseDouble(keyValuePairs, "MHS av", 0.00) * 1000;
 
                     //seen both MHS 5s and MHS 1s
                     if (keyValuePairs.ContainsKey("MHS 5s"))
-                        newDevice.CurrentHashrate = double.Parse(keyValuePairs["MHS 5s"], CultureInfo.InvariantCulture) * 1000;
+                        newDevice.CurrentHashrate = TryToParseDouble(keyValuePairs, "MHS 5s", 0.00) * 1000;
                     else if (keyValuePairs.ContainsKey("MHS 1s"))
-                        newDevice.CurrentHashrate = double.Parse(keyValuePairs["MHS 1s"], CultureInfo.InvariantCulture) * 1000;
-
-                    if (keyValuePairs.ContainsKey("Accepted")) //check required for bfgminer
-                    {
-                        //personally seen this need extra handling with a user
-                        stringValue = keyValuePairs["Accepted"];
-                        intValue = 0;
-                        if (int.TryParse(stringValue, NumberStyles.Number,
-                             CultureInfo.InvariantCulture, out intValue))
-                            newDevice.AcceptedShares = intValue;
-                    }
-
-                    if (keyValuePairs.ContainsKey("Rejected")) //check required for bfgminer
-                    {
-                        //personally seen this need extra handling with a user
-                        stringValue = keyValuePairs["Rejected"];
-                        intValue = 0;
-                        if (int.TryParse(stringValue, NumberStyles.Number,
-                             CultureInfo.InvariantCulture, out intValue))
-                            newDevice.RejectedShares = intValue;
-                    }
-
-                    if (keyValuePairs.ContainsKey("Hardware Errors")) //check required for bfgminer
-                    {
-                        //personally seen this need extra handling with a user
-                        stringValue = keyValuePairs["Hardware Errors"];
-                        intValue = 0;
-                        if (int.TryParse(stringValue, NumberStyles.Number,
-                             CultureInfo.InvariantCulture, out intValue))
-                            newDevice.HardwareErrors = intValue;
-                    }
-
-                    if (keyValuePairs.ContainsKey("Utility")) //check required for bfgminer
-                    {
-                        //personally seen this need extra handling with a user
-                        stringValue = keyValuePairs["Utility"];
-                        double doubleValue = 0.00;
-                        if (double.TryParse(stringValue, NumberStyles.Number,
-                             CultureInfo.InvariantCulture, out doubleValue))
-                            newDevice.Utility = doubleValue;
-                    }
-
-                    newDevice.PoolIndex = -1;
-                    if (keyValuePairs.ContainsKey("Last Share Pool")) //check required for bfgminer
-                    {
-                        //personally seen this need extra handling with a user
-                        stringValue = keyValuePairs["Last Share Pool"];
-                        intValue = 0;
-                        if (int.TryParse(stringValue, NumberStyles.Number,
-                             CultureInfo.InvariantCulture, out intValue))
-                            newDevice.PoolIndex = intValue;
-                    }
-
+                        newDevice.CurrentHashrate = TryToParseDouble(keyValuePairs, "MHS 1s", 0.00) * 1000;
+                    
+                    newDevice.AcceptedShares = TryToParseInt(keyValuePairs, "Accepted", 0);                    
+                    newDevice.RejectedShares = TryToParseInt(keyValuePairs, "Rejected", 0);                    
+                    newDevice.HardwareErrors = TryToParseInt(keyValuePairs, "Hardware Errors", 0);                    
+                    newDevice.Utility = TryToParseDouble(keyValuePairs, "Utility", 0.00);                    
+                    newDevice.PoolIndex = TryToParseInt(keyValuePairs, "Last Share Pool", -1);
+                    
                     deviceInformation.Add(newDevice);
                 }
             }
+        }
+
+        //the RPC API returns numbers formatted en-US, e.g. 1,000.00
+        //specify CultureInfo.InvariantCulture for parsing or unhandled exceptions will
+        //occur on other locales
+        //can test for this with:
+        //Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+        //Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+        private static int TryToParseInt(Dictionary<string, string> keyValuePairs, string key, int defaultValue)
+        {
+            int result = defaultValue;
+
+            if (keyValuePairs.ContainsKey(key))
+            {
+                string stringValue = keyValuePairs[key];
+                int intValue;
+                if (int.TryParse(stringValue, NumberStyles.Number, CultureInfo.InvariantCulture, out intValue))
+                    result = intValue;
+            }
+
+            return result;
+        }
+
+        private static double TryToParseDouble(Dictionary<string, string> keyValuePairs, string key, double defaultValue)
+        {
+            double result = defaultValue;
+
+            if (keyValuePairs.ContainsKey(key))
+            {
+                string stringValue = keyValuePairs[key];
+                double doubleValue;
+                if (double.TryParse(stringValue, NumberStyles.Number, CultureInfo.InvariantCulture, out doubleValue))
+                    result = doubleValue;
+            }
+
+            return result;
         }
     }
 }
