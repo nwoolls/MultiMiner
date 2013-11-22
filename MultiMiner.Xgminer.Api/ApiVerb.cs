697ab1671331e1958fe7fe8b230dd75b9e9b3c91
Nate Woolls <nwoolls@gmail.com>
2013-11-22 02:21:00 -0500

    Added support for the DEVDETAILS RPC call

--- MultiMiner.Xgminer.Api/ApiVerb.cs
+++ MultiMiner.Xgminer.Api/ApiVerb.cs
@@ -3,6 +3,7 @@
     public static class ApiVerb
     {
         public const string Devs = "devs";
+        public const string DevDetails = "devdetails";
         public const string Quit = "quit";
         public const string Summary = "summary";
     }
