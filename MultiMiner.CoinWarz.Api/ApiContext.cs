e2e1ee16b86edf75c1a302b632d465f7399f4a37
Nathanial Woolls <nwoolls@gmail.com>
2013-11-21 21:31:01 -0500

    Implement a wrapper for the CoinWarz API

---
+++ MultiMiner.CoinWarz.Api/ApiContext.cs
@@ -0,0 +1,47 @@
+﻿using MultiMiner.Coin.Api;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace MultiMiner.CoinWarz.Api
+{
+    public class ApiContext : IApiContext
+    {
+        private readonly string apiKey;
+        public ApiContext(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        public IEnumerable<CoinInformation> GetCoinInformation(string userAgent = "",
+            BaseCoin profitabilityBasis = BaseCoin.Bitcoin)
+        {
+            WebClient client = new WebClient();
+            if (!string.IsNullOrEmpty(userAgent))
+                client.Headers.Add("user-agent", userAgent);
+
+            string apiUrl = String.Format(@"http://www.coinwarz.com/v1/api/profitability/?apikey={0}&algo=all", apiKey);
+
+            string jsonString = client.DownloadString(apiUrl);
+
+            JObject jsonObject = JObject.Parse(jsonString);
+            
+            JArray jsonArray = jsonObject.Value<JArray>("Data");
+
+            List<CoinInformation> result = new List<CoinInformation>();
+
+            foreach (JToken jToken in jsonArray)
+            {
+                CoinInformation coinInformation = new CoinInformation();
+                coinInformation.PopulateFromJson(jToken);
+                if (coinInformation.Difficulty > 0)
+                    //only add coins with valid info since the user may be basing
+                    //strategies on Difficulty
+                    result.Add(coinInformation);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MultiMiner.CoinWarz.Api/CoinInformationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using MultiMiner.Coin.Api;
+using Newtonsoft.Json.Linq;
+
+namespace MultiMiner.CoinWarz.Api
+{
+    public static class CoinInformationExtensions
+    {
+        public static void PopulateFromJson(this CoinInformation coinInformation, JToken jToken)
+        {
+            coinInformation.Symbol = jToken.Value<string>("CoinTag");
+            coinInformation.Name = jToken.Value<string>("CoinName");
+            coinInformation.Algorithm = jToken.Value<string>("Algorithm");
+            coinInformation.CurrentBlocks = jToken.Value<int>("BlockCount");
+            coinInformation.Difficulty = jToken.Value<double>("Difficulty");
+            coinInformation.Reward = jToken.Value<double>("BlockReward");
+            coinInformation.Price = jToken.Value<double>("ExchangeRate");
+            coinInformation.Exchange = jToken.Value<string>("Exchange");
+            coinInformation.Profitability = jToken.Value<double>("ProfitRatio");
+            coinInformation.AdjustedProfitability = jToken.Value<double>("ProfitRatio");
+            coinInformation.AverageProfitability = jToken.Value<double>("AvgProfitRatio");
+        }
+    }
+}
