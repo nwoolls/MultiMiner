cc862c0f3020a1eeaaebcd7c3987668d5c933c87
Nathanial Woolls <nwoolls@gmail.com>
2013-11-29 02:28:16 -0500

    Hide the toolbar grip in the Coins dialog

--- MultiMiner.Win/CoinsForm.Designer.cs
+++ MultiMiner.Win/CoinsForm.Designer.cs
@@ -83,6 +83,7 @@
             // 
             // toolStrip1
             // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addCoinButton,
             this.removeCoinButton,
