5d820efbe3aa637d90d635a1fdf42ab5c3da6fd3
Nathanial Woolls <nwoolls@gmail.com>
2013-11-29 11:31:28 -0500

    Fix formatting of dates when the year comes first (JP)

--- MultiMiner.Win/Extensions/DateTimeExtensions.cs
+++ MultiMiner.Win/Extensions/DateTimeExtensions.cs
@@ -3,14 +3,19 @@ using System.Globalization;
 
 namespace MultiMiner.Win.Extensions
 {
-    static class DateTimeExtensions
+    public static class DateTimeExtensions
     {
         public static string ToReallyShortDateString(this DateTime dateTime)
         {
             //short date no year
-            string shortDateValue = dateTime.ToShortDateString();
-            int lastIndex = shortDateValue.LastIndexOf(CultureInfo.CurrentCulture.DateTimeFormat.DateSeparator);
-            return shortDateValue.Remove(lastIndex);
+            string shortDateString = dateTime.ToShortDateString();
+
+            //year could be at beginning (JP) or end (EN)
+            string dateSeparator = CultureInfo.CurrentCulture.DateTimeFormat.DateSeparator;
+            string value1 = dateTime.Year + dateSeparator;
+            string value2 = dateSeparator + dateTime.Year;
+
+            return shortDateString.Replace(value1, String.Empty).Replace(value2, String.Empty);
         }
     }
 }
