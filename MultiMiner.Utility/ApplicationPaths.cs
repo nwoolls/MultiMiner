2ff0df6800e53b54bf163d8546413bcc5d9975fa
Nathanial Woolls <nwoolls@gmail.com>
2013-12-22 00:28:17 -0500

    Support for running the app in a "portable" mode

--- MultiMiner.Utility/ApplicationPaths.cs
+++ MultiMiner.Utility/ApplicationPaths.cs
@@ -7,8 +7,31 @@ namespace MultiMiner.Utility
     {
         public static string AppDataPath()
         {
-            string rootPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            return Path.Combine(rootPath, "MultiMiner");
+            //if running in "portable" mode
+            if (IsRunningInPortableMode())
+            {
+                //store files in the working directory rather than AppData
+                return GetWorkingDirectory();
+            }
+            else
+            {
+                string rootPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                return Path.Combine(rootPath, "MultiMiner");
+            }
+        }
+
+        //simply check for a file named "portable" in the same folder
+        private static bool IsRunningInPortableMode()
+        {
+            string assemblyDirectory = GetWorkingDirectory();
+            string portableFile = Path.Combine(assemblyDirectory, "portable");
+            return File.Exists(portableFile);
+        }
+
+        private static string GetWorkingDirectory()
+        {
+            string assemblyFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            return Path.GetDirectoryName(assemblyFilePath);
         }
     }
 }
