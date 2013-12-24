c751e3b535efed2faf5bc9af2f1c5a7a0e383b63
Nathanial Woolls <nwoolls@gmail.com>
2013-12-23 22:37:48 -0500

    Don't allow running multiple instances (but leave the option)

--- MultiMiner.Win/Program.cs
+++ MultiMiner.Win/Program.cs
@@ -1,19 +1,41 @@
 ﻿using MultiMiner.Utility;
+using MultiMiner.Win.Configuration;
 using System;
 using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace MultiMiner.Win
 {
     static class Program
     {
+        private static string appGuid = "E78D8F73-9241-4BE2-800D-DCD01259BB97";
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
+        {
+            using (Mutex mutex = new Mutex(false, "Global\\" + appGuid))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    PathConfiguration pathConfig = new PathConfiguration();
+                    pathConfig.LoadPathConfiguration();
+                    ApplicationConfiguration appConfig = new ApplicationConfiguration();
+                    appConfig.LoadApplicationConfiguration(pathConfig.SharedConfigPath);
+                    if (!appConfig.AllowMultipleInstances)
+                        return;
+                }
+
+                RunApplication();
+            }
+        }
+
+        private static void RunApplication()
         {
             if (OSVersionPlatform.GetConcretePlatform() == PlatformID.Unix)
             {
@@ -28,8 +50,6 @@ namespace MultiMiner.Win
                     return true;
                 };
             }
-
-
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
