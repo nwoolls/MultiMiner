17fd8f3ffb14f73e2dcd7a41650ee1d013916294
Nathanial Woolls <nwoolls@gmail.com>
2014-01-24 18:57:28 -0500

    Update buttons whether local or remote

--- MultiMiner.Win/MainForm.cs
+++ MultiMiner.Win/MainForm.cs
@@ -688,14 +688,14 @@ namespace MultiMiner.Win
             else
             {
                 UpdateMiningButtonsForRemote();
-
-                //disable actions in the menu (for now, until remote commands are done)
-                advancedMenuItem.Enabled = this.selectedRemoteInstance == null;
-                settingsButton.Enabled = this.selectedRemoteInstance == null;
-                toolsToolStripMenuItem.Enabled = this.selectedRemoteInstance == null;
-                advancedToolStripMenuItem.Enabled = this.selectedRemoteInstance == null;
-                actionsToolStripMenuItem.Enabled = this.selectedRemoteInstance == null;
             }
+
+            //disable actions in the menu (for now, until remote commands are done)
+            advancedMenuItem.Enabled = this.selectedRemoteInstance == null;
+            settingsButton.Enabled = this.selectedRemoteInstance == null;
+            toolsToolStripMenuItem.Enabled = this.selectedRemoteInstance == null;
+            advancedToolStripMenuItem.Enabled = this.selectedRemoteInstance == null;
+            actionsToolStripMenuItem.Enabled = this.selectedRemoteInstance == null;
         }
 
         private void UpdateMiningButtonsForRemote()
