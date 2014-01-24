34ee8808609b93325b9b4afb03d21e073bbb56b4
Nathanial Woolls <nwoolls@gmail.com>
2014-01-24 14:41:15 -0500

    Introduced a method for getting the workgroup name

---
+++ MultiMiner.Utility/Networking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Management;
+
+namespace MultiMiner.Utility
+{
+    public class Networking
+    {
+        public static string GetWorkGroup()
+        {
+            ManagementObject computer_system = new ManagementObject(
+                        string.Format(
+                        "Win32_ComputerSystem.Name='{0}'",
+                        Environment.MachineName));
+
+            object result = computer_system["Workgroup"];
+            return result.ToString();
+        }
+    }
+}
