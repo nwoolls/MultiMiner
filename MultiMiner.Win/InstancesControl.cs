eaf2d0afd7255c302b8dd179e5abfa064b8169b0
Nathanial Woolls <nwoolls@gmail.com>
2014-01-25 17:43:01 -0500

    Show total network hashrate in the instances TreeView

--- MultiMiner.Win/InstancesControl.cs
+++ MultiMiner.Win/InstancesControl.cs
@@ -11,6 +11,8 @@ namespace MultiMiner.Win
     public partial class InstancesControl : MessageBoxFontUserControl
     {
         private const string ThisPCText = "This PC";
+        private const string NetworkText = "Network";
+
         public class InstanceSorter : IComparer
         {
             // compare between two tree nodes
@@ -37,6 +39,8 @@ namespace MultiMiner.Win
         public event SelectedInstanceChangedHandler SelectedInstanceChanged;
 
         public List<Instance> Instances { get; set; }
+
+        private Dictionary<Instance, Remoting.Server.Data.Transfer.Machine> instanceMachines = new Dictionary<Instance, Remoting.Server.Data.Transfer.Machine>();
 
         public InstancesControl()
         {
@@ -82,21 +86,22 @@ namespace MultiMiner.Win
             treeView1.Nodes[0].ExpandAll();
 
             if (isThisPc)
-            {
                 treeView1.SelectedNode = node;
-            }
         }
 
         public void UnregisterInstance(Instance instance)
         {
             treeView1.Nodes[0].Nodes.RemoveByKey(instance.IpAddress);
             Instances.Remove(instance);
+            instanceMachines.Remove(instance);
+            RefreshNetworkTotals();
         }
 
         public void UnregisterInstances()
         {
             Instances.Clear();
             treeView1.Nodes[0].Nodes.Clear();
+            instanceMachines.Clear();
         }
 
         private string GetMachineName(string ipAddress)
@@ -105,7 +110,7 @@ namespace MultiMiner.Win
             return GetMachineName(instance);
         }
 
-        private string GetMachineName(Instance instance)
+        private static string GetMachineName(Instance instance)
         {
             string result = instance.MachineName;
             bool isThisPc = result.Equals(Environment.MachineName);
@@ -118,6 +123,10 @@ namespace MultiMiner.Win
         {
             if (ipAddress.Equals("localhost"))
                 ipAddress = Instances.Single(i => i.MachineName.Equals(Environment.MachineName)).IpAddress;
+
+            Instance instance = Instances.SingleOrDefault(i => i.IpAddress.Equals(ipAddress));
+            if (instance != null)
+                instanceMachines[instance] = machine;
 
             TreeNode[] nodes = treeView1.Nodes[0].Nodes.Find(ipAddress, false);
             if (nodes.Length > 0)
@@ -145,6 +154,38 @@ namespace MultiMiner.Win
                 {
                     nodes[0].Text = GetMachineName(ipAddress);
                 }
+            }
+
+            RefreshNetworkTotals();
+        }
+
+        private void RefreshNetworkTotals()
+        {
+            double totalScrypt = instanceMachines.Values.Sum(m => m.TotalScryptHashrate);
+            double totalSha256 = instanceMachines.Values.Sum(m => m.TotalSha256Hashrate);
+
+            if ((totalSha256 > 0) && (totalScrypt > 0))
+            {
+                treeView1.Nodes[0].Text = String.Format("{0} ({1}, {2})",
+                    NetworkText,
+                    totalSha256.ToHashrateString(),
+                    totalScrypt.ToHashrateString());
+            }
+            else if (totalSha256 > 0)
+            {
+                treeView1.Nodes[0].Text = String.Format("{0} ({1})",
+                    NetworkText,
+                    totalSha256.ToHashrateString());
+            }
+            else if (totalScrypt > 0)
+            {
+                treeView1.Nodes[0].Text = String.Format("{0} ({1})",
+                    NetworkText,
+                    totalScrypt.ToHashrateString());
+            }
+            else
+            {
+                treeView1.Nodes[0].Text = NetworkText;
             }
         }
 
