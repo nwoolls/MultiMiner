9a728b410682ae5f535f1b9b3bc7e27600eb0456
Nathanial Woolls <nwoolls@gmail.com>
2014-01-26 19:18:15 -0500

    Make Networking.GetWorkGroup() cross-platform

--- MultiMiner.Utility/Networking.cs
+++ MultiMiner.Utility/Networking.cs
@@ -7,13 +7,30 @@ namespace MultiMiner.Utility
     {
         public static string GetWorkGroup()
         {
-            ManagementObject computer_system = new ManagementObject(
-                        string.Format(
-                        "Win32_ComputerSystem.Name='{0}'",
-                        Environment.MachineName));
-
-            object result = computer_system["Workgroup"];
-            return result.ToString();
+            string result = String.Empty;
+            if (OSVersionPlatform.GetConcretePlatform() == PlatformID.MacOSX)
+            {
+                //OS X
+                string configFilePath = @"/Library/Preferences/SystemConfiguration/com.apple.smb.server.plist";
+                PlistParser parser = new PlistParser(configFilePath);
+                result = parser["Workgroup"].ToString();
+            }
+            else if (OSVersionPlatform.GetGenericPlatform() == PlatformID.Unix)
+            {
+                //Linux
+                string configFilePath = @"/etc/samba/smb.conf";
+                IniFileParser parser = new IniFileParser(configFilePath);
+                result = parser.GetValue("Global", "Workgroup");
+            }
+            else
+            {
+                //Windows
+                using (ManagementObject managementObject = new ManagementObject(String.Format("Win32_ComputerSystem.Name='{0}'", Environment.MachineName)))
+                {
+                    result = managementObject["Workgroup"].ToString();
+                }
+            }
+            return result;
         }
     }
 }
---
+++ MultiMiner.Utility/PlistParser.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace MultiMiner.Utility
+{
+
+    public class PlistParser : Dictionary<string, object>
+    {
+        public PlistParser()
+        {
+        }
+
+        public PlistParser(string file)
+        {
+            Load(file);
+        }
+
+        public void Load(string file)
+        {
+            Clear();
+
+            XDocument doc = XDocument.Load(file);
+            XElement plist = doc.Element("plist");
+            XElement dict = plist.Element("dict");
+
+            var dictElements = dict.Elements();
+            Parse(this, dictElements);
+        }
+
+        private void Parse(PlistParser dict, IEnumerable<XElement> elements)
+        {
+            for (int i = 0; i < elements.Count(); i += 2)
+            {
+                XElement key = elements.ElementAt(i);
+                XElement val = elements.ElementAt(i + 1);
+
+                dict[key.Value] = ParseValue(val);
+            }
+        }
+
+        private List<object> ParseArray(IEnumerable<XElement> elements)
+        {
+            List<dynamic> list = new List<dynamic>();
+            foreach (XElement e in elements)
+            {
+                object one = ParseValue(e);
+                list.Add(one);
+            }
+
+            return list;
+        }
+
+        private object ParseValue(XElement val)
+        {
+            switch (val.Name.ToString())
+            {
+                case "string":
+                    return val.Value;
+                case "integer":
+                    return int.Parse(val.Value);
+                case "real":
+                    return float.Parse(val.Value);
+                case "true":
+                    return true;
+                case "false":
+                    return false;
+                case "dict":
+                    PlistParser plist = new PlistParser();
+                    Parse(plist, val.Elements());
+                    return plist;
+                case "array":
+                    List<dynamic> list = ParseArray(val.Elements());
+                    return list;
+                default:
+                    throw new ArgumentException("Unsupported");
+            }
+        }
+    }
+}
