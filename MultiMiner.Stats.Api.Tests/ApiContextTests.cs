e861ff19d560f5264df2a85e538136633e82b6a8
Nathanial Woolls <nwoolls@gmail.com>
2014-01-29 23:47:43 -0500

    Additional work on unit tests, integration tests, and code coverage

---
+++ MultiMiner.Stats.Api.Tests/ApiContextTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiMiner.Stats.Api.Tests
+{
+    [TestClass]
+    public class ApiContextTests
+    {
+        [TestMethod]
+        public void SubmitMinerStatistics_Succeeds()
+        {
+            //arrange
+            Machine machineStat = new Machine() 
+            { 
+                MinerVersion = "1.0.0", 
+                Name = Environment.MachineName 
+            };
+
+            //act
+            ApiContext.SubmitMinerStatistics("http://multiminerstats.azurewebsites.net/api/", machineStat);   
+        }
+    }
+}
---
+++ MultiMiner.Utility.Tests/UnzipperTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+
+namespace MultiMiner.Utility.Tests
+{
+    [TestClass]
+    public class UnzipperTests
+    {
+        [TestMethod]
+        public void UnzipFileToFolder_Succeeds()
+        {
+            //arrange
+            string tempPath = Path.GetTempPath();
+            string executingPath = Path.GetFullPath("App_Data");
+            const string fileName = "Unzipper-Test.zip";
+            string tempFilePath = Path.Combine(tempPath, Path.ChangeExtension(fileName, "txt"));
+            string zipFilePath = Path.Combine(executingPath, fileName);
+            File.Delete(tempFilePath);
+
+            //act
+            Unzipper.UnzipFileToFolder(zipFilePath, tempPath);
+
+            //assert
+            Assert.IsTrue(File.Exists(zipFilePath));
+            string contents = File.ReadAllText(tempFilePath);
+            Assert.IsTrue(contents.Equals("Unzipper Test"));
+
+            File.Delete(tempFilePath);            
+        }
+    }
+}
