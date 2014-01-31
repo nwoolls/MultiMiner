1536e76b5ddfde8cd90e82f127a5dca8588d5470
Nathanial Woolls <nwoolls@gmail.com>
2014-01-31 00:00:15 -0500

    Add support for verifying a list of potential miner endpoints

--- MultiMiner.Xgminer.Discovery.Tests/MinerFinderTests.cs
+++ MultiMiner.Xgminer.Discovery.Tests/MinerFinderTests.cs
@@ -11,12 +11,25 @@ namespace MultiMiner.Xgminer.Discovery.Tests
         [TestMethod]
         public void MinerFinder_FindsMiners()
         {
-            const int times = 30;
+            const int times = 10;
             for (int i = 0; i < times; i++)
             {
                 List<IPEndPoint> miners = MinerFinder.Find("192.168.0.29-100", 4028, 4029);
                 Assert.IsTrue(miners.Count >= 4);
             }
+        }
+
+        [TestMethod]
+        public void MinerFinder_ChecksMiners()
+        {
+            List<IPEndPoint> miners = MinerFinder.Find("192.168.0.29-100", 4028, 4029);
+            int goodCount = miners.Count;
+
+            miners.Add(new IPEndPoint(IPAddress.Parse("10.0.0.1"), 1000));
+
+            int checkCount = MinerFinder.Check(miners).Count;
+
+            Assert.IsTrue(checkCount == goodCount);
         }
     }
 }
--- MultiMiner.Xgminer.Discovery/MinerFinder.cs
+++ MultiMiner.Xgminer.Discovery/MinerFinder.cs
@@ -14,26 +14,46 @@ namespace MultiMiner.Xgminer.Discovery
                 throw new ArgumentException();
 
             List<IPEndPoint> endpoints = PortScanner.Find(ipRange, startingPort, endingPort);
-
             List<IPEndPoint> miners = new List<IPEndPoint>();
 
             foreach (IPEndPoint ipEndpoint in endpoints)
-            {
-                ApiContext context = new ApiContext(ipEndpoint.Port, ipEndpoint.Address.ToString());
-                string response = null;
-                try
-                {
-                    response = context.GetResponse("version");
-                }
-                catch (Exception ex)
-                {
-                    response = null;
-                }
-                if (!String.IsNullOrEmpty(response))
+                if (EndpointIsMiner(ipEndpoint))
                     miners.Add(ipEndpoint);
-            }
 
             return miners;
+        }
+
+        public static List<IPEndPoint> Check(List<IPEndPoint> possibleMiners)
+        {
+            List<IPEndPoint> actualMiners = new List<IPEndPoint>();
+
+            foreach (IPEndPoint possibleMiner in possibleMiners)
+                if (EndpointIsMiner(possibleMiner))
+                    actualMiners.Add(possibleMiner);
+
+            return actualMiners;
+        }
+
+        private static bool EndpointIsMiner(IPEndPoint ipEndpoint)
+        {
+            bool endpointIsMiner = false;
+
+            ApiContext context = new ApiContext(ipEndpoint.Port, ipEndpoint.Address.ToString());
+
+            string response = null;
+            try
+            {
+                response = context.GetResponse("version");
+            }
+            catch (Exception ex)
+            {
+                response = null;
+            }
+
+            if (!String.IsNullOrEmpty(response))
+                endpointIsMiner = true;
+
+            return endpointIsMiner;
         }
     }
 }
