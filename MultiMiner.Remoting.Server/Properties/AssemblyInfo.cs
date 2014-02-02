15720dadaa0954c941656941c0a6d390fadb2ce8
Nathanial Woolls <nwoolls@gmail.com>
2014-02-02 18:20:42 -0500

    Version info updated

--- MultiMiner.Remoting.Server/Properties/AssemblyInfo.cs
+++ MultiMiner.Remoting.Server/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.6.0.136")]
-[assembly: AssemblyVersion("2.6.0.136")]
-[assembly: AssemblyFileVersion("2.6.0.136")]
+// [assembly: AssemblyVersion("2.6.0.138")]
+[assembly: AssemblyVersion("2.6.0.138")]
+[assembly: AssemblyFileVersion("2.6.0.138")]
