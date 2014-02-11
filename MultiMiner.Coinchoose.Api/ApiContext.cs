b0eb3d125cce00852f911b705deed1076e3d9b3d
Nathanial Woolls <nwoolls@gmail.com>
2014-02-11 17:40:30 -0500

    Fix / clarify comments

--- MultiMiner.Coinchoose.Api/ApiContext.cs
+++ MultiMiner.Coinchoose.Api/ApiContext.cs
@@ -17,7 +17,7 @@ namespace MultiMiner.CoinChoose.Api
             {
                 WebRequest w = base.GetWebRequest(uri);
                 //default is 100s - far too long for our API calls
-                //if Azure is being flakey we don't want calls taking 100s to timeout
+                //if API is being flakey we don't want calls taking 100s to timeout
                 //lets go with 10s
                 w.Timeout = 10 * 1000;
                 return w;
