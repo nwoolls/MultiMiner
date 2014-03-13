6368dd046d41afde9db6166e767d24674068dcb6
Nathanial Woolls <nwoolls@gmail.com>
2014-03-13 09:41:53 -0400

    Version info updated

--- MultiMiner.Win/Properties/AssemblyInfo.cs
+++ MultiMiner.Win/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.7.7.170")]
-[assembly: AssemblyVersion("2.7.7.170")]
-[assembly: AssemblyFileVersion("2.7.7.170")]
+// [assembly: AssemblyVersion("2.7.8.171")]
+[assembly: AssemblyVersion("2.7.8.171")]
+[assembly: AssemblyFileVersion("2.7.8.171")]
