9572daf3e5eb77918036b07b0bcd19fd964159ef
Nathanial Woolls <nwoolls@gmail.com>
2014-03-14 16:39:46 -0400

    Update (and futureproof) the copyright

---
+++ MultiMiner.Win/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System;
+
+namespace MultiMiner.Win.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static DateTime GetCompileDate(this Assembly assembly)
+        {
+            return RetrieveLinkerTimestamp(assembly.Location);
+        }
+
+        // http://www.codinghorror.com/blog/2005/04/determining-build-date-the-hard-way.html
+        private static DateTime RetrieveLinkerTimestamp(string filePath)
+        {
+            const int peHeaderOffset = 60;
+            const int linkerTimestampOffset = 8;
+            var b = new byte[2048];
+            System.IO.FileStream s = null;
+            try
+            {
+                s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                s.Read(b, 0, 2048);
+            }
+            finally
+            {
+                if (s != null)
+                    s.Close();
+            }
+            var dt = new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(BitConverter.ToInt32(b, BitConverter.ToInt32(b, peHeaderOffset) + linkerTimestampOffset));
+            return dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+        }
+    }
+}
--- MultiMiner.Win/Forms/AboutForm.cs
+++ MultiMiner.Win/Forms/AboutForm.cs
@@ -4,6 +4,8 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
+using MultiMiner.Win.Extensions;
+using System.Reflection;
 
 namespace MultiMiner.Win.Forms
 {
@@ -16,7 +18,15 @@ namespace MultiMiner.Win.Forms
 
         private void AboutForm_Load(object sender, EventArgs e)
         {
+            PopulateCopyright();
             PopulateAppVersions();
+        }
+
+        private void PopulateCopyright()
+        {
+            DateTime compileDate = Assembly.GetExecutingAssembly().GetCompileDate();
+            const string source = "(C) 2013";
+            licenseTextBox.Text = licenseTextBox.Text.Replace(source, String.Format("{0} - {1}", source, compileDate.Year));
         }
 
         private void PopulateAppVersions()
