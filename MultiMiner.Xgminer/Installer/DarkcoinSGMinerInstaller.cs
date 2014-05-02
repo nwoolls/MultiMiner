3011edfbebd3e04e99452b3fa6a82feb22c247d3
Nathanial Woolls <nwoolls@gmail.com>
2014-05-02 04:21:00 -0400

    Introduce an installer for the Darkcoin X11 build of SGMiner

---
+++ MultiMiner.Xgminer/Installer/DarkcoinSGMinerInstaller.cs
@@ -0,0 +1,21 @@
+﻿namespace MultiMiner.Xgminer.Installer
+{
+    public class DarkcoinSGMinerInstaller : BaseInstaller, IMinerInstaller
+    {
+        public void InstallMiner(string destinationFolder)
+        {
+            InstallMiner(GetMinerDownloadUrl(), destinationFolder);
+        }
+
+        public string GetAvailableMinerVersion()
+        {
+            //unknown - no way to tell
+            return null;
+        }
+
+        private string GetMinerDownloadUrl()
+        {
+            return "http://www.darkcoin.io/downloads/darkcoin-sgminer-windows.zip";
+        }
+    }
+}
