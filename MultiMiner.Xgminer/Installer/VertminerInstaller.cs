b4535d02d5bc0faf08a3193bd15c4b1a6a45382c
Nathanial Woolls <nwoolls@gmail.com>
2014-05-02 03:54:51 -0400

    Introducate an installer for Vertminer

---
+++ MultiMiner.Xgminer/Installer/VertminerInstaller.cs
@@ -0,0 +1,25 @@
+﻿namespace MultiMiner.Xgminer.Installer
+{
+    public class VertminerInstaller : BaseInstaller, IMinerInstaller
+    {
+        public void InstallMiner(string destinationFolder)
+        {
+            InstallMiner(GetMinerDownloadUrl(), destinationFolder);
+        }
+
+        public string GetAvailableMinerVersion()
+        {
+            return GetAvailableMinerVersion(GetMinerDownloadUrl());
+        }
+
+        private string GetMinerDownloadUrl()
+        {
+            const string htmlRoot = "http://vertcoin.org";
+            const string htmlPath = "/";
+            const string filePattern = @".*<a href=""/downloads/(vertminer-.+?.zip)"">Windows</a>";
+            const string downloadPath = "downloads/";
+
+            return GetDownloadUrl(htmlRoot, htmlPath, filePattern, downloadPath);
+        }
+    }
+}
