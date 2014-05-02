cced4cd8e33567c91f63288a7c3fe14066c05241
Nathanial Woolls <nwoolls@gmail.com>
2014-05-02 04:14:53 -0400

    Introducate an installer for the Scrypt-Jane build of Kalroth's CGMiner

---
+++ MultiMiner.Xgminer/Installer/KalrothSJCGMinerInstaller.cs
@@ -0,0 +1,25 @@
+﻿namespace MultiMiner.Xgminer.Installer
+{
+    public class KalrothSJCGMinerInstaller : BaseInstaller, IMinerInstaller
+    {
+        public void InstallMiner(string destinationFolder)
+        {
+            InstallMiner(GetMinerDownloadUrl(), destinationFolder);
+        }
+
+        public string GetAvailableMinerVersion()
+        {
+            return GetAvailableMinerVersion(GetMinerDownloadUrl());
+        }
+
+        private string GetMinerDownloadUrl()
+        {
+            const string htmlRoot = "https://sites.google.com/site/rmdavidson";
+            const string htmlPath = "/";
+            const string filePattern = @".*<a href=""https://sites.google.com/site/rmdavidson/(cgminer-.+?-kalroth-sj0.03.zip)";
+            const string downloadPath = "";
+
+            return GetDownloadUrl(htmlRoot, htmlPath, filePattern, downloadPath);
+        }
+    }
+}
