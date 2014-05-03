bcfa77100d0ef93529988feec91e5f8efc3e7439
Nathanial Woolls <nwoolls@gmail.com>
2014-05-03 11:06:13 -0400

    Automatically backup and restore configurations

--- MultiMiner.Utility/Serialization/ConfigurationReaderWriter.cs
+++ MultiMiner.Utility/Serialization/ConfigurationReaderWriter.cs
@@ -7,6 +7,8 @@ namespace MultiMiner.Utility.Serialization
 {
     public static class ConfigurationReaderWriter
     {
+        private const string BackupExtension = ".bak";
+
         public static T ReadConfiguration<T>(string fileName, string rootElement = null) where T : new()
         {
             if (File.Exists(fileName))
@@ -19,6 +21,7 @@ namespace MultiMiner.Utility.Serialization
 
                 using (TextReader reader = new StreamReader(fileName))
                 {
+
                     T result;
                     try
                     {
@@ -26,15 +29,54 @@ namespace MultiMiner.Utility.Serialization
                     }
                     catch (InvalidOperationException ex)
                     {
-                        MessageBox.Show(String.Format("The file {0} was 0 bytes (likely due to a previous crash).\n\nDefault settings for this file have been loaded.", 
-                            Path.GetFileName(fileName)), "Invalid Configuration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        result = new T();
+                        //close reader so we can delete the file
+                        reader.Close();
+
+                        result = HandleZeroByteConfiguration<T>(fileName, rootElement);
                     }
+
+                    //backup settings
+                    BackupConfiguration(fileName);
+
                     return result;
                 }
             }
 
             return new T();
+        }
+
+        private static void BackupConfiguration(string fileName)
+        {
+            string backupFileName = fileName + BackupExtension;
+            File.Delete(backupFileName);
+            File.Copy(fileName, backupFileName);
+        }
+
+        private static T HandleZeroByteConfiguration<T>(string fileName, string rootElement) where T : new()
+        {
+            string backupFileName = fileName + BackupExtension;
+
+            T result;
+
+            //file was 0 bytes - due to crash on startup
+            if (File.Exists(backupFileName))
+            {
+                //restore backup file
+                MessageBox.Show(String.Format("The file {0} was 0 bytes (likely due to a crash on startup).\n\nA previous backup will be restored.",
+                    Path.GetFileName(fileName)), "Invalid Configuration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                File.Delete(fileName);
+                File.Copy(backupFileName, fileName);
+                result = ConfigurationReaderWriter.ReadConfiguration<T>(fileName, rootElement);
+            }
+            else
+            {
+                //load defaults
+                MessageBox.Show(String.Format("The file {0} was 0 bytes (likely due to a crash on startup).\n\nDefault settings for this file will be loaded.",
+                    Path.GetFileName(fileName)), "Invalid Configuration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                result = new T();
+            }
+
+            return result;
         }
 
         public static void WriteConfiguration(object source, string fileName, string rootElement = null)
