3fe9d0dd4eea61523fe43d63aeb89775a6859896
Nathanial Woolls <nwoolls@gmail.com>
2014-05-19 23:24:16 -0400

    Version info updated

--- MultiMiner.CoinWarz/Properties/AssemblyInfo.cs
+++ MultiMiner.CoinWarz/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.0.209")]
-[assembly: AssemblyVersion("3.0.0.209")]
-[assembly: AssemblyFileVersion("3.0.0.209")]
+// [assembly: AssemblyVersion("3.0.0.210")]
+[assembly: AssemblyVersion("3.0.0.210")]
+[assembly: AssemblyFileVersion("3.0.0.210")]
