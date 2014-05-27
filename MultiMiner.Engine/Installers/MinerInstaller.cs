cb5a335aeae4d57913487d2e02b7afbe14650c4a
Nathanial Woolls <nwoolls@gmail.com>
2014-05-26 20:33:16 -0400

    Bug fix for fuzzing version (debug only)

--- MultiMiner.Engine/Installers/MinerInstaller.cs
+++ MultiMiner.Engine/Installers/MinerInstaller.cs
@@ -84,7 +84,7 @@ namespace MultiMiner.Engine.Installers
             if (!String.IsNullOrEmpty(version))
             {
                 Version fuzzVersion = new Version(version);
-                version = new Version(fuzzVersion.Major, fuzzVersion.Minor - 1, fuzzVersion.Build).ToString();
+                version = new Version(fuzzVersion.Major, Math.Max(0, fuzzVersion.Minor - 1), fuzzVersion.Build).ToString();
             }
 #endif
 
