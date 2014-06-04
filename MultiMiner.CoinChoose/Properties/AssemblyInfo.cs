f4b97395f3486477c074ec6856c60886353e4be5
Nathanial Woolls <nwoolls@gmail.com>
2014-06-04 00:36:06 -0400

    Version info updated

--- MultiMiner.CoinChoose/Properties/AssemblyInfo.cs
+++ MultiMiner.CoinChoose/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.1.0.219")]
-[assembly: AssemblyVersion("3.1.0.219")]
-[assembly: AssemblyFileVersion("3.1.0.219")]
+// [assembly: AssemblyVersion("3.1.1.230")]
+[assembly: AssemblyVersion("3.1.1.230")]
+[assembly: AssemblyFileVersion("3.1.1.230")]
