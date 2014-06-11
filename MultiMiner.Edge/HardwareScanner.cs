3e909fca51ceb445620d439c4aee6cdf13fcce25
Nathanial Woolls <nwoolls@gmail.com>
2014-06-11 02:17:37 -0400

    Initial assembly to simplify accessing MultiMiner assemblies from Node.js

---
+++ MultiMiner.Edge/HardwareScanner.cs
@@ -0,0 +1,25 @@
+﻿using MultiMiner.Services;
+using MultiMiner.Xgminer.Data;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MultiMiner.Edge
+{
+    public class HardwareScanner
+    {
+
+        public async Task<object> GetDevices(object input)
+        {
+            DevicesService devicesService = new DevicesService();
+
+            string workingPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            List<Device> devices = devicesService.GetDevices(Path.Combine(workingPath,"Miners", "bfgminer", "bfgminer.exe"));
+
+            return JsonConvert.SerializeObject(devices);
+        }
+    }
+}
