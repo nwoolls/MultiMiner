b553937dcb26e629929955e6d11505cb83f8e733
Nathanial Woolls <nwoolls@gmail.com>
2014-08-21 23:28:39 -0400

    Version info updated

--- MultiMiner.Xgminer.Api/Properties/AssemblyInfo.cs
+++ MultiMiner.Xgminer.Api/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.4.0.247")]
-[assembly: AssemblyVersion("3.4.0.247")]
-[assembly: AssemblyFileVersion("3.4.0.247")]
+// [assembly: AssemblyVersion("3.4.0.249")]
+[assembly: AssemblyVersion("3.4.0.249")]
+[assembly: AssemblyFileVersion("3.4.0.249")]
