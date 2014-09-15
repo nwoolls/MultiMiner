c13cd94e741040ea4cf54e0dfc4fb6b858fb5f48
Nathanial Woolls <nwoolls@gmail.com>
2014-09-14 22:07:03 -0400

    Introduce a read-only TextBox

---
+++ MultiMiner.Utility/Forms/ReadOnlyTextBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MultiMiner.Utility.Forms
+{
+    public class ReadOnlyTextBox : TextBox
+    {
+        [DllImport("user32.dll")]
+        static extern bool HideCaret(IntPtr hWnd);
+
+        public ReadOnlyTextBox()
+        {
+            this.ReadOnly = true;
+            this.GotFocus += TextBoxGotFocus;
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void TextBoxGotFocus(object sender, EventArgs args)
+        {
+            HideCaret(this.Handle);
+        }
+    }
+}
