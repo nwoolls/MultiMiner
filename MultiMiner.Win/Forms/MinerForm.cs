3b24dcad719c50ef76793269becb74386e26f4b0
Nathanial Woolls <nwoolls@gmail.com>
2014-11-06 01:27:05 -0500

    Fix casing of check

--- MultiMiner.Win/Forms/MinerForm.cs
+++ MultiMiner.Win/Forms/MinerForm.cs
@@ -7839,7 +7839,7 @@ namespace MultiMiner.Win.Forms
 
             string response = apiContext.RestartMining();
 
-            return !response.ToLower().Contains("STATUS=E");
+            return !response.ToLower().Contains("STATUS=E".ToLower());
         }
 
         private void SetNetworkDevicePool(int poolIndex)
