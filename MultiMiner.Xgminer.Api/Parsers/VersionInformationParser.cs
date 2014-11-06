899d5d95efd9b4735506d6cf73d5eff340f9946f
Nathanial Woolls <nwoolls@gmail.com>
2014-11-05 22:48:10 -0500

    Work around incomatibility in SGMiner 4.0 with the CGMiner RPC API

--- MultiMiner.Xgminer.Api/Parsers/VersionInformationParser.cs
+++ MultiMiner.Xgminer.Api/Parsers/VersionInformationParser.cs
@@ -17,7 +17,20 @@ namespace MultiMiner.Xgminer.Api.Parsers
 
             keyValuePairs = GetDictionaryFromTextChunk(responseParts[1]);
 
-            versionInformation.MinerVersion = keyValuePairs["CGMiner"];
+            string key = "CGMiner";
+            if (keyValuePairs.ContainsKey(key))
+                versionInformation.MinerVersion = keyValuePairs[key];
+            else
+            {
+                // SGMiner 4.0 broke compatibility with the CGMiner RPC API
+                // Version 5.0 fixed the issue - this work-around is for 4.0
+                // Request :"version"
+                // Response:"STATUS=S,When=1415068731,Code=22,Msg=SGMiner versions,Description=sgminer 4.1.0|VERSION,SGMiner=4.1.0,API=3.1|\u0000"
+                key = "SGMiner";
+                if (keyValuePairs.ContainsKey(key))
+                    versionInformation.MinerVersion = keyValuePairs[key];
+            }
+
             versionInformation.ApiVersion = keyValuePairs["API"];
         }
     }
