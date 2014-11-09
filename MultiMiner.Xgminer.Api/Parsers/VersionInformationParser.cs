cbd796eb8c83fad44f5eca601c5f6667238fda1d
Nathanial Woolls <nwoolls@gmail.com>
2014-11-09 18:19:53 -0500

    Handle locked down Network Devices returning blank RPC API data

--- MultiMiner.Xgminer.Api/Parsers/VersionInformationParser.cs
+++ MultiMiner.Xgminer.Api/Parsers/VersionInformationParser.cs
@@ -12,26 +12,30 @@ namespace MultiMiner.Xgminer.Api.Parsers
 
             Dictionary<string, string> keyValuePairs = GetDictionaryFromTextChunk(responseParts[0]);
 
-            versionInformation.Name = keyValuePairs["Msg"].Replace(" versions", String.Empty);
-            versionInformation.Description = keyValuePairs["Description"];
-
-            keyValuePairs = GetDictionaryFromTextChunk(responseParts[1]);
-
-            string key = "CGMiner";
-            if (keyValuePairs.ContainsKey(key))
-                versionInformation.MinerVersion = keyValuePairs[key];
-            else
+            //check for key-value pairs, seen Count == 0 with user API logs
+            if (keyValuePairs.Count > 0)
             {
-                // SGMiner 4.0 broke compatibility with the CGMiner RPC API
-                // Version 5.0 fixed the issue - this work-around is for 4.0
-                // Request :"version"
-                // Response:"STATUS=S,When=1415068731,Code=22,Msg=SGMiner versions,Description=sgminer 4.1.0|VERSION,SGMiner=4.1.0,API=3.1|\u0000"
-                key = "SGMiner";
+                versionInformation.Name = keyValuePairs["Msg"].Replace(" versions", String.Empty);
+                versionInformation.Description = keyValuePairs["Description"];
+
+                keyValuePairs = GetDictionaryFromTextChunk(responseParts[1]);
+
+                string key = "CGMiner";
                 if (keyValuePairs.ContainsKey(key))
                     versionInformation.MinerVersion = keyValuePairs[key];
-            }
+                else
+                {
+                    // SGMiner 4.0 broke compatibility with the CGMiner RPC API
+                    // Version 5.0 fixed the issue - this work-around is for 4.0
+                    // Request :"version"
+                    // Response:"STATUS=S,When=1415068731,Code=22,Msg=SGMiner versions,Description=sgminer 4.1.0|VERSION,SGMiner=4.1.0,API=3.1|\u0000"
+                    key = "SGMiner";
+                    if (keyValuePairs.ContainsKey(key))
+                        versionInformation.MinerVersion = keyValuePairs[key];
+                }
 
-            versionInformation.ApiVersion = keyValuePairs["API"];
+                versionInformation.ApiVersion = keyValuePairs["API"];
+            }
         }
     }
 }
