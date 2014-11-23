073f8d817b14cb03072e8188301532adac563b5a
Nathanial Woolls <nwoolls@gmail.com>
2014-11-23 03:31:51 -0500

    Update version info

--- MultiMiner.Coinbase/Properties/AssemblyInfo.cs
+++ MultiMiner.Coinbase/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.6.12.327")]
-[assembly: AssemblyVersion("3.6.12.327")]
-[assembly: AssemblyFileVersion("3.6.12.327")]
+// [assembly: AssemblyVersion("3.7.1.328")]
+[assembly: AssemblyVersion("3.7.1.328")]
+[assembly: AssemblyFileVersion("3.7.1.328")]
--- MultiMiner.WhatMine/Properties/AssemblyInfo.cs
+++ MultiMiner.WhatMine/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.6.3.307")]
-[assembly: AssemblyVersion("3.6.3.307")]
-[assembly: AssemblyFileVersion("3.6.3.307")]
+// [assembly: AssemblyVersion("3.7.1.328")]
+[assembly: AssemblyVersion("3.7.1.328")]
+[assembly: AssemblyFileVersion("3.7.1.328")]
