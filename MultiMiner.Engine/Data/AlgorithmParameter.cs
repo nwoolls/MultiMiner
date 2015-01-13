feeec0158062dc5291208141925df29f775c07cb
Nathanial Woolls <nwoolls@gmail.com>
2015-01-12 23:28:09 -0500

    Add support for, and default most GPU algos to, SGMiner 5

--- MultiMiner.Engine/Data/AlgorithmParameter.cs
+++ MultiMiner.Engine/Data/AlgorithmParameter.cs
@@ -3,14 +3,14 @@
     class AlgorithmParameter
     {
         public const string Scrypt = "--scrypt";
-        public const string AlgorithmNScrypt = "--algorithm nscrypt";
         public const string ScryptJane = "--scrypt-jane";
-        public const string KernelQuarkcoin = "--kernel quarkcoin";
-        public const string KernelGroestcoin = "--kernel groestlcoin";
-        public const string Keccak = "--keccak";
-        public const string KernelX11Mod = "--kernel x11mod";
-        public const string KernelX13Mod = "--kernel x13mod";
-        public const string PoolAlgorithmBitBlockOld = "--pool-algorithm bitblockold";
-        public const string PoolAlgorithmX14Old = "--pool-algorithm x14old";
+        public const string AlgorithmScryptNFactor11 = "--algorithm scrypt --nfactor 11";
+        public const string AlgorithmQuarkcoin = "--algorithm quarkcoin";
+        public const string AlgorithmGroestlcoin = "--algorithm groestlcoin";
+        public const string AlgorithmKeccak = "--algorithm keccak";
+        public const string AlgorithmX11 = "--algorithm x11";
+        public const string AlgorithmX13 = "--algorithm x13";
+        public const string AlgorithmX14 = "--algorithm x14";
+        public const string AlgorithmX15 = "--algorithm x15";
     }
 }
--- MultiMiner.Engine/Data/MinerNames.cs
+++ MultiMiner.Engine/Data/MinerNames.cs
@@ -5,6 +5,7 @@
         public const string BFGMiner = "BFGMiner";
         public const string CGMiner = "CGMiner";
         public const string SGMiner = "SGMiner";
+        public const string SGMiner5 = "SGMiner5";
         public const string SPHSGMiner = "SPHSGMiner";
         public const string KalrothSJCGMiner = "KalrothSJCGMiner";
         public const string LBSPHSGMiner = "LBSPHSGMiner";
