302e7dd02bb1490bd3977c0b963f798d863c72af
Nathanial Woolls <nwoolls@gmail.com>
2015-01-30 00:06:13 -0500

    Refactor MultiMiner.Utility namespaces

--- MultiMiner.Utility/OS/NativeMethods.cs
+++ MultiMiner.Utility/OS/NativeMethods.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 
-namespace MultiMiner.Utility
+namespace MultiMiner.Utility.OS
 {
     static class NativeMethods
     {
