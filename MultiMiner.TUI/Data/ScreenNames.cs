c8116384fd3fca8890dd7574ba152ea026a1d106
Nathanial Woolls <nwoolls@gmail.com>
2015-02-08 04:20:57 -0500

    Introduce a History screen

--- MultiMiner.TUI/Data/ScreenNames.cs
+++ MultiMiner.TUI/Data/ScreenNames.cs
@@ -5,5 +5,6 @@
         public const string Main = "main";
         public const string Repl = "repl";
         public const string ApiLog = "apilog";
+        public const string History = "history";
     }
 }
