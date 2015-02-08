09511465dafa8214c07abbff0c7a58369b70afa5
Nathanial Woolls <nwoolls@gmail.com>
2015-02-08 01:58:37 -0500

    Log unhandled exceptions

--- MultiMiner.TUI/Program.cs
+++ MultiMiner.TUI/Program.cs
@@ -1,11 +1,21 @@
-﻿namespace MultiMiner.TUI
+﻿using System;
+using System.Diagnostics;
+
+namespace MultiMiner.TUI
 {
     //http://broadcast.oreilly.com/2010/08/understanding-c-text-mode-games.html
     class Program
     {
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             new MinerApplication().Run();
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            EventLog.WriteEntry("Application Error", (e.ExceptionObject as Exception).ToString(), EventLogEntryType.Error, 1000);
         }
     }
 }
