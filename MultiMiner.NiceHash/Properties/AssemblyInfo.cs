4905f77afadc9e6823a3f25d3c8db78f2196c540
Nate Woolls <nwoolls@gmail.com>
2018-02-04 00:15:42 -0500

    Update version info

--- MultiMiner.NiceHash/Properties/AssemblyInfo.cs
+++ MultiMiner.NiceHash/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.2.2.378")]
-[assembly: AssemblyVersion("4.2.2.378")]
-[assembly: AssemblyFileVersion("4.2.2.378")]
+// [assembly: AssemblyVersion("4.2.3.379")]
+[assembly: AssemblyVersion("4.2.3.379")]
+[assembly: AssemblyFileVersion("4.2.3.379")]
